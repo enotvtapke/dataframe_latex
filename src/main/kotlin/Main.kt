b00794be3df461e8344a0439584f45bb35868bae
enotvtapke <a.stupnikov05@gmail.com>
2024-12-27 13:37:25 +0100

    Copy necessary code from internals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,80 +1,45 @@
 @file:ImportDataSchema(
-    "JetBrains",
-    "jetbrains.json",
-)
-@file:ImportDataSchema(
-    "Test",
-    "test.json",
-)
-@file:ImportDataSchema(
     "MyTest",
     "my_test.json",
-)Save
+)
 
 import org.jetbrains.kotlinx.dataframe.AnyFrame
 import org.jetbrains.kotlinx.dataframe.AnyRow
-import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.annotations.ImportDataSchema
-import org.jetbrains.kotlinx.dataframe.api.add
-import org.jetbrains.kotlinx.dataframe.api.dropNulls
-import org.jetbrains.kotlinx.dataframe.api.filter
 import org.jetbrains.kotlinx.dataframe.api.map
 import org.jetbrains.kotlinx.dataframe.api.take
-import org.jetbrains.kotlinx.dataframe.api.toListOf
 import org.jetbrains.kotlinx.dataframe.impl.asList
-import org.jetbrains.kotlinx.dataframe.io.readCSV
-import org.jetbrains.kotlinx.dataframe.io.readJson
 import org.jetbrains.kotlinx.dataframe.io.toJson
 import org.jetbrains.kotlinx.dataframe.size
-import java.io.StringWriter
 import java.math.BigDecimal
 import kotlin.collections.map
 
 fun main() {
-    data class Passenger(
-        val survived: Int,
-        val homedest: String?,
-        val age: Int,
-        val lastName: String
-    )
-    val df = JetBrains.readJson("jetbrains.json")
+//    val df = JetBrains.readJson("jetbrains.json")
     val df1 = MyTest.readJson("my_test.json")
     print(df1.toLatex())
     print(df1.Title[0].toString())
-//    print(df.repos[0].nodeId.toLatex(10, 100))
-    val passengers1 = DataFrame.readCSV("titanic.csv", delimiter=';')
-//    println(passengers1.toLatex(3))
-//    println(df[0].toLatex(3))
-    val passengers = DataFrame.readCSV("titanic.csv", delimiter=';')
-        .add(Passenger::lastName) { "name"<String>().split(",").last() }
-        .dropNulls(Passenger::age)
-        .filter {
-            it[Passenger::survived] == 1 &&
-                    it[Passenger::homedest]?.endsWith("NY") == true &&
-                    it[Passenger::age] in 10..20
-        }
-        .toListOf<Passenger>()
-//    print(passengers)
 }
 
-fun AnyFrame.toLatex(limit: Int = 10, limit2: Int = 100): String =
-    StringWriter().let {
-        it.appendLine("\\begin{tabular}{${"|c".repeat(this.size().ncol) + "|"}}")
+fun String.escape() = this.replace("_", "\\_")
 
-        it.appendLine("\\hline")
-        it.appendLine(columns().map { it.name() }.joinToString(separator = " & ", postfix = " \\\\"))
-        it.appendLine("\\hline")
-        it.appendLine(this.take(limit).map { row ->
+fun AnyFrame.toLatex(limit: Int = 10, limit2: Int = 100): String =
+    buildString {
+        appendLine("\\begin{tabular}{${"|c".repeat(this@toLatex.size().ncol) + "|"}}")
+        appendLine("\\hline")
+        appendLine(columns().map { it.name().escape() }.joinToString(separator = " & ", postfix = " \\\\"))
+        appendLine("\\hline")
+        appendLine(this@toLatex.take(limit).map { row ->
             val values = row.values().take(limit2).map {
                 when (it) {
-                    is AnyRow -> it.toJson() // TODO
+                    is AnyRow -> it.toJson().escape()
                     is AnyFrame -> it.toLatex()
-                    else -> it
+                    else -> renderValueToString(it).escape()
                 }
-            }.map { renderValueToString(it) }
+            }
             values.joinToString(separator = " & ", postfix = " \\\\\n")
         }.joinToString(separator = "\\hline\n", postfix = "\\hline"))
-        it.appendLine("\\end{tabular}")
+        appendLine("\\end{tabular}")
     }.toString()
 
 fun Any.asArrayAsListOrNull(): List<*>? =
